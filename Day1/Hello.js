00f9f56062ec3f099fe47e1594d1c51cee00773b
AswinAsok <aswinasokofficial@gmail.com>
2020-08-17 15:34:38 +0530

    Day1-JSBasics

---
+++ Day1/Hello.js
@@ -0,0 +1,18 @@
+var name = "Aswin";
+var name1 = new String("Aswin")
+
+
+console.log(name1.length)
+
+var n = 10;
+var num =new  Number(10);
+
+var s = true;
+var s1 = new Boolean(true);
+
+
+console.log(`Hello World ${name} ${n} `);
+
+k=74
+
+console.log(k);
