45d125fd9d8e5e15957ae520082dec1f8f64b5a6
AswinAsok <aswinasokofficial@gmail.com>
2020-08-24 14:48:18 +0530

    Sever-Creation

---
+++ sampleserver.js
@@ -0,0 +1,23 @@
+// var http  = require('http')
+
+// http.createServer(sever).listen(3000);
+
+// function sever(req,res){
+//     res.write('Packapeer')
+//     res.end()
+// }
+
+
+var http  = require('http')
+var fs = require('fs')
+
+http.createServer(function(req,res){
+    fs.readFile('sample html.html',function(err,data){
+        
+        res.writeHead(200,{"Content-type":"text/html"})
+        res.write(data)
+        res.end()
+    })
+    
+}).listen(3000);
+
