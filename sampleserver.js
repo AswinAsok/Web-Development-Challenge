707700b20a23a52ffde26432a1c5aa849e2682e1
AswinAsok <aswinasokofficial@gmail.com>
2020-08-27 14:51:29 +0530

    Store The Data Through Server

--- sampleserver.js
+++ sampleserver.js
@@ -10,14 +10,44 @@
 
 var http  = require('http')
 var fs = require('fs')
+var url = require('url')
 
 http.createServer(function(req,res){
-    fs.readFile('sample html.html',function(err,data){
-        
+
+    var q = url.parse(req.url,true) 
+    console.log(q.pathname); 
+    
+    if(q.pathname==='/'){
+ 
+    fs.readFile('serhtml.html',function(err,data){
+                
         res.writeHead(200,{"Content-type":"text/html"})
         res.write(data)
         res.end()
-    })
-    
-}).listen(3000);
+        })
+    }
+    else if(q.pathname==='/signup'){
+        fs.readFile("form.html",(err,data) => { 
+        res.writeHead(200,{"Content-type":"text/html"})
+        res.write(data)
+        res.end();
+        })
+    }
+    else if(q.pathname === "/signupaction"){
+
+
+        console.log(q.query);
+
+        res.writeHead(200,{"Content-type":"text/html"})
+
+        res.write("<h1>"+q.query.fullname+"</h>")
+        res.end()
+    }
+    else{
+        res.writeHead(404,{"Content-type":"text/html"})
+        res.write("Error")
+        res.end()
+    }
+
+}).listen(7500,() => console.log("Server is Runinng"))
 
